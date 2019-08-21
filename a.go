f40f8a48f6d199b3ad0babe249d2df7bdcc85935
rstorr <reuben@codelingo.io>
2019-08-21 18:42:22 +1200

    com

--- a.go
+++ a.go
@@ -14,3 +14,13 @@ func modifiedFuncName1() {
 	check(err)
 	fmt.Printf("%d bytes: %s\n", n, string(b))
 }
+
+func modifiedFuncName1() {
+	f, err := os.Open("/tmp/test.txt") //ISSUE
+	check(err)
+	//defer f.Close()
+	b := make([]byte, 5)
+	n, err := f.Read(b)
+	check(err)
+	fmt.Printf("%d bytes: %s\n", n, string(b))
+}
