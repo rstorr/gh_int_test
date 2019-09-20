244fa4d66faffe90d8999736bf18b94733d6ec9c
rstorr <reuben@codelingo.io>
2019-09-20 15:23:26 +1200

    open file. don't close it.

---
+++ a_no_defer.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func aFunc() {
+	f, err := os.Open("/tmp/test.txt") //ISSUE
+	check(err)
+	b := make([]byte, 5)
+	n, err := f.Read(b)
+	check(err)
+	fmt.Printf("%d bytes: %s\n", n, string(b))
+}
